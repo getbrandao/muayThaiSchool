08b090259b1659f88a6190ebb125b7b9251bb9c9
Gustavo Brandao <gustavobrandao@bsd.com.br>
2017-01-30 12:18:49 -0300

    Configuration to paginate and adjust controller

--- anmal/app/controllers/home_controller.rb
+++ anmal/app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
    before_action :find_article, only: [:show]
 
   def index
-    @articles = Article.all.order("created_at DESC")
+   @per_page = params[:per_page] || 10
+   @query = Article.all.order("created_at DESC").ransack(params[:q])
+   @articles = @query.result.paginate(:page => params[:page], :per_page => @per_page)
+   respond_with(@articles)
   end
 
   def show
