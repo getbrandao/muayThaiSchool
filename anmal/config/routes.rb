3a742ddb702f3c9cad9eaa8b089e4f5a4819e42a
Gustavo Brandao <gustavobrandao@bsd.com.br>
2017-02-03 12:52:44 -0300

    Adjust on routes and CSS.

--- anmal/config/routes.rb
+++ anmal/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
   devise_for :users, :skip => [:registrations]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root 'home#index'
-  resources :home, only: [:show]
+  root to: 'articles#index'
+  resources :articles, only: [:show, :index, :search]
 end
